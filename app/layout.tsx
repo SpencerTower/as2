5c70e3a28506e9b76b83dc23e75f3924da0cfc2b
Rob Bearman <robbear@hyperfine.com>
2023-11-14 08:45:32 -0800

    Initial Next.js and shadcn/ui files

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Atlasphere',
+  description: 'Place. Time. Content. The map is the canvas.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
